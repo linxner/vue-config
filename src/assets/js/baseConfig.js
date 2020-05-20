93bedbaee1f43cc93186215f7fe2ba4340a8938c
linxner <342709615@qq.com>
2020-05-20 11:38:16 +0800

    20200520

---
+++ src/assets/js/baseConfig.js
@@ -0,0 +1,3 @@
+export const RequestUrl = "https://aip.baidubce.com/rpc/2.0/ai_custom/v1/classification/abcmouse";
+export const APIKey='YTfIBy2sBLxlXIcf2H6CvZva'
+export const SecretKey='7Xl7x9pb1xGyNBlPf3ndY60d9Hj9kBkC'
