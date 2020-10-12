d99a81c4ab146729668043313e32437b0acc69d1
linxner <342709615@qq.com>
2020-10-12 11:05:44 +0800

    20201012

--- src/assets/js/share.js
+++ src/assets/js/share.js
@@ -15,30 +15,30 @@ function wechatShare() {
       timestamp: res.timestamp,
       nonceStr: res.nonceStr,
       signature: res.signature,
-      jsApiList: ['openWXSearchPage', 'onMenuShareAppMessage', 'onMenuShareTimeline', 'hideOptionMenu']
+      jsApiList: ['openWXSearchPage', 'updateAppMessageShareData', 'updateTimelineShareData', 'hideOptionMenu']
 
     });
     wx.error(function (res) {
       console.log(res)
     });
   });
-  var data = {
-    title: "收下你的2020年健康关键词",
-    desc: "2020年，你健康自由了吗？",
-    link: 'https://littleidea.elloworld.cn/we_search',
-    imgUrl: 'https://littleidea.elloworld.cn/we-search/img/share.png'
+  let data = {
+    title: "我是拜耳人，我为“敢想|勇为”代言",
+    desc: "做乘风破浪的拜耳人！",
+    link: 'https://idea.elloworld.cn/bayer/',
+    imgUrl: 'https://idea.elloworld.cn/bayer/img/share.png'
   }
   console.log(data);
 
   wx.ready(function () {
     wx.checkJsApi({
-      jsApiList: ['openWXSearchPage','getNetworkType', 'previewImage'],
+      jsApiList: ['openWXSearchPage', 'getNetworkType', 'previewImage'],
       success: function (res) {
         console.log(JSON.stringify(res));
       }
     });
 
-    wx.onMenuShareTimeline({
+    wx.updateTimelineShareData({
       title: data.title,
       link: data.link,
       imgUrl: data.imgUrl,
@@ -50,7 +50,7 @@ function wechatShare() {
       }
     });
 
-    wx.onMenuShareAppMessage({
+    wx.updateAppMessageShareData({
       title: data.title,
       desc: data.desc,
       link: data.link,
